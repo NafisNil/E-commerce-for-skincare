51318edfeb8052d8d89d9876c6813427d6c9ad87
nafisnil <nafis.zaman1.an@gmail.com>
2025-03-25 17:05:51 +0600

    first_com

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\FrontendController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [FrontendController::class, 'index'])->name('index');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
